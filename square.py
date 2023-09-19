a07afaad89397719b23072ef34cab31fc990c31d
Alexader Ershov <ershovsanek.centr@yandex.ru>
2023-09-20 00:35:28 +0300

    docs: add square.py docstrings

--- square.py
+++ square.py
@@ -1,7 +1,23 @@
 
 def area(a):
+    """Returns an area of given square
+
+    Args:
+        a (int/float): side of the square
+
+    Returns:
+        int/float: area of the square
+    """
     return a * a
 
 
 def perimeter(a):
+    """Returns a perimeter of given square
+
+    Args:
+        a (int/float): side of the square
+
+    Returns:
+        int/float: perimeter of the square
+    """
     return 4 * a
