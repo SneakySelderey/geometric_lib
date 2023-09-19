80d4a10edad5c7346b639644b08a46ce4c855699
Alexader Ershov <ershovsanek.centr@yandex.ru>
2023-09-20 00:18:17 +0300

    docs: add rectangle.py docstrings

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,23 @@
-def area(a, b): 
+def area(a, b):
+    """return an area of given rectangle
+
+    Args:
+        a (int/float): one side of the rectangle
+        b (int/float): neighboring side of the rectangle
+
+    Returns:
+        int/float: area of the rectangle
+    """
     return a * b 
 
-def perimeter(a, b): 
+def perimeter(a, b):
+    """returns a perimeter of given rectangle
+
+    Args:
+        a (int/float): one side of the rectangle
+        b (int/float): neighboring side of the rectangle
+
+    Returns:
+        int/float: perimeter of the rectangle
+    """
     return 2 * (a + b) 
