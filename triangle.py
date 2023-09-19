5fa16fbc1ffabd0dc742219828f3a19685fdceeb
Alexader Ershov <ershovsanek.centr@yandex.ru>
2023-09-20 00:23:30 +0300

    docs: add triangle.py docstrings

--- triangle.py
+++ triangle.py
@@ -1,5 +1,24 @@
-def area(a, h): 
+def area(a, h):
+    """return an area of given triangle
+
+    Args:
+        a (int/float): side of the triangle
+        h (int/float): height of the triangle that drops on side "a"
+
+    Returns:
+        int/float: area of the triangle
+    """
     return a * h / 2 
 
-def perimeter(a, b, c): 
+def perimeter(a, b, c):
+    """return a perimeter of given triangle
+
+    Args:
+        a (int/float): first side of the triangle
+        b (int/float): second side of the triangle
+        c (int/float): third side of the triangle
+
+    Returns:
+        int/float: perimeter of the triangle
+    """
     return a + b + c 
