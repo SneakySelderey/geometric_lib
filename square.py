3b1ce7d640015dbcf78c92f15c4f4468efb6c162
Alexander Ershov <ershovsanek.centr@yandex.ru>
2023-10-25 12:44:33 +0300

    test: add tests for square.py

--- square.py
+++ square.py
@@ -1,3 +1,42 @@
+import unittest
+
+
+class RectangleTestCase(unittest.TestCase):
+    """Class of tests for rectangle.py functions
+
+    Args:
+        unittest (TestCase): parent class
+    """
+    def set_up(self):
+        """Set up fucntion
+        """
+        pass
+
+
+    def test_area_zero_sides(self):
+        """Test function for zero sides area case
+        """
+        self.assertEqual(get_square_area(0), 0)
+
+
+    def test_perimeter_zero_sides(self):
+        """Test function for zero sides perimeter case
+        """
+        self.assertEqual(get_square_perimeter(0), 0)
+
+
+    def test_area_float_sides(self):
+        """Test function for float sides area case
+        """
+        self.assertAlmostEqual(get_square_area(6.82), 46.5124, 4)
+
+
+    def test_perimeter_float_sides(self):
+        """Test function for float sides perimeter case
+        """
+        self.assertEqual(get_square_perimeter(6.82), 27.28, 4)
+
+
 
 def get_square_area(a):
     """Returns an area of given square
