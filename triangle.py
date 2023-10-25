7cb0ac8b26d48e6d1b0930427cdea66be2305ae5
Alexander Ershov <ershovsanek.centr@yandex.ru>
2023-10-25 14:04:17 +0300

    test: add triangle.py tests

--- triangle.py
+++ triangle.py
@@ -1,3 +1,80 @@
+import unittest
+
+
+class TriangleTestCase(unittest.TestCase):
+    """Class of tests for rectangle.py functions
+
+    Args:
+        unittest (TestCase): parent class
+    """
+    def set_up(self):
+        """Set up fucntion
+        """
+        pass
+
+
+    def test_area_zero_side_or_height(self):
+        """Test function for zero side or height area case
+        """
+        for x in [0, 5]:
+            for y in [0, 5]:
+                if [x, y] != [5, 5]:
+                    self.assertRaises(ValueError, get_triangle_area, x, y)
+
+
+    def test_perimeter_zero_side(self):
+        """Test function for zero sides perimeter case
+        """
+        for x in [0, 5, 10]:
+            for y in [0, 5, 10]:
+                for z in [0, 5, 10]:
+                    if x * y * z == 0:
+                        self.assertRaises(ValueError, get_triangle_perimeter, x, y, z)
+
+
+    def test_area_float_sides_height(self):
+        """Test function for float sides and float height area case
+        """
+        self.assertAlmostEqual(get_triangle_area(6.82, 3.57), 12.1737, 4)
+
+
+    def test_perimeter_float_sides(self):
+        """Test function for float sides perimeter case
+        """
+        self.assertEqual(get_triangle_perimeter(6.82, 3.57, 7.25), 17.64, 4)
+
+
+    def test_area_negative_values(self):
+        """Test function for negative values area case
+        """
+        for x in [-1, 5]:
+            for y in [-1, 5]:
+                if [x, y] != [5, 5]:
+                    self.assertRaises(ValueError, get_triangle_area, x, y)
+
+
+    def test_perimeter_negative_values(self):
+        """Test function for negative values area case
+        """
+        for x in [-1, 5, 10]:
+            for y in [-1, 5, 10]:
+                for z in [-1, 5, 10]:
+                    if x < 0 or y < 0 or z < 0:
+                        self.assertRaises(ValueError, get_triangle_perimeter, x, y, z)
+
+
+    def test_area_int_values(self):
+        """Test function for generic values area case
+        """
+        self.assertEqual(get_triangle_area(10, 4), 20)
+
+
+    def test_perimeter_int_values(self):
+        """Test function for generic values perimeter case
+        """
+        self.assertEqual(get_triangle_perimeter(10, 4, 6), 20)
+
+
 def get_triangle_area(a, h):
     """Returns an area of given triangle
 
@@ -8,7 +85,7 @@ def get_triangle_area(a, h):
     Returns:
         float: area of the triangle
     """
-    return a * h / 2 
+    return a * h / 2
 
 def get_triangle_perimeter(a, b, c):
     """Returns a perimeter of given triangle
@@ -21,4 +98,4 @@ def get_triangle_perimeter(a, b, c):
     Returns:
         int/float: perimeter of the triangle
     """
-    return a + b + c 
+    return a + b + c
