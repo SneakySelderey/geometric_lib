f2e9a6ed324950ffee259483a74446e5a5612e73
Alexander Ershov <ershovsanek.centr@yandex.ru>
2023-10-25 12:26:34 +0300

    refactor: test classes moved to their files

--- circle.py
+++ circle.py
@@ -1,4 +1,53 @@
 import math
+import unittest
+
+
+class CircleTestCase(unittest.TestCase):
+    """Class of tests for circle.py functions
+
+    Args:
+        unittest (TestCase): parent class
+    """
+    def set_up(self):
+        """Set up fucntion
+        """
+        pass
+
+
+    def test_area_zero_rad(self):
+        """Test function for zero radius area case
+        """
+        self.assertEqual(get_circle_area(0), 0)
+
+
+    def test_circumference_zero_rad(self):
+        """Test function for zero radius circumference case
+        """
+        self.assertEqual(get_circle_perimeter(0), 0)
+
+
+    def test_area_negative_rad(self):
+        """Test function for negative radius area case
+        """
+        self.assertRaises(ValueError, get_circle_area, -1)
+
+
+    def test_circumference_negative_rad(self):
+        """Test function for negative radius circumference case
+        """
+        self.assertRaises(ValueError, get_circle_perimeter, -1)
+
+
+    def test_area_float_rad(self):
+        """Test function for float radius area case
+        """
+        self.assertAlmostEqual(get_circle_area(3.57), 40.039284, 6)
+
+
+    def test_circumference_float_rad(self):
+        """Test function for float radius circumference case
+        """
+        self.assertAlmostEqual(get_circle_perimeter(3.57), 22.43097, 5)
 
 
 def get_circle_area(r):
--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,56 @@
+import unittest
+
+
+class RectangleTestCase(unittest.TestCase):
+    """Class of tests for rectangle.py functions
+
+    Args:
+        unittest (TestCase): parent class
+    """
+    def set_up(self):
+        """Set up fucntion
+        """
+        pass
+
+
+    def test_area_zero_sides(self):
+        """Test function for zero sides area case
+        """
+        self.assertEqual(get_rectangle_area(0, 0), 0)
+
+
+    def test_perimeter_zero_sides(self):
+        """Test function for zero sides perimeter case
+        """
+        self.assertEqual(get_rectangle_perimeter(0, 0), 0)
+
+
+    def test_area_zero_side(self):
+        """Test function for zero side area case
+        """
+        self.assertEqual(get_rectangle_area(0, 5), 0)
+        self.assertEqual(get_rectangle_area(5, 0), 0)
+
+
+    def test_perimeter_zero_side(self):
+        """Test function for zero side perimeter case
+        """
+        self.assertEqual(get_rectangle_perimeter(0, 5), 0)
+        self.assertEqual(get_rectangle_perimeter(5, 0), 0)
+
+
+    def test_area_float_sides(self):
+        """Test function for float sides area case
+        """
+        self.assertAlmostEqual(get_rectangle_area(4.23, 6.82), 28.8486, 4)
+
+
+    def test_perimeter_float_sides(self):
+        """Test function for float sides perimeter case
+        """
+        self.assertEqual(get_rectangle_perimeter(4.23, 6.82), 22.1, 4)
+
+
 def get_rectangle_area(a, b):
     """Returns an area of given rectangle
 
